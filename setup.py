fa42a9574c713b94afe9c24aa50ad58612ea00a4
Denis Demidov <dennis.demidov@gmail.com>
2015-01-19 12:18:00 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup, Extension
+import os, sys
+
+pyver = sys.version_info
+
+#----------------------------------------------------------------------------
+def find_file(filename, search_dirs):
+    for dirname in search_dirs:
+        for root, dirs, files in os.walk(dirname):
+            for f in files:
+                if filename in f:
+                    return dirname
+            for d in dirs:
+                if filename in d:
+                    return dirname
+            if filename in root:
+                return dirname
+    return False
+
+
+#----------------------------------------------------------------------------
+def boost_python_lib():
+    library_dirs = [
+            '/usr/local/lib64/',
+            '/usr/lib64/',
+            '/usr/local/lib/',
+            '/usr/lib/',
+            '/opt/local/lib/'
+            ]
+
+    boost_python = "boost_python-%s.%s" % (pyver[0], pyver[1])
+    if find_file("lib" + boost_python, library_dirs):
+        return boost_python
+
+    if pyver >= (3, ):
+        boost_python = "boost_python-py%s%s" % (pyver[0], pyver[1])
+        if find_file("lib" + boost_python, library_dirs):
+            return boost_python
+        boost_python = "boost_python%s" % pyver[0]
+        if find_file("lib" + boost_python, library_dirs):
+            return boost_python
+
+    return "boost_python"
+
+
+#----------------------------------------------------------------------------
+setup(
+        name='python-ev3dev',
+        version='0.0.1',
+        description='Python language bindings for ev3dev',
+        author='Denis Demidov',
+        author_email='dennis.demidov@gmail.com',
+        license='GPLv2',
+        url='https://github.com/ddemidov/evdev-lang-python',
+        include_package_data=True,
+        ext_modules=[
+            Extension('ev3dev',
+                language='c++',
+                sources=['pyev3dev.cpp', 'ev3dev.cpp'],
+                include_dirs=['.'],
+                libraries=[boost_python_lib()],
+                extra_compile_args=['-O2', '-std=c++11']
+                )
+            ]
+)
+
