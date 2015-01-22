02b52b4b545657f7beb6d1e76489af8b97ba32be
Denis Demidov <dennis.demidov@gmail.com>
2015-01-22 21:13:50 +0300

    Explicitly set the correct platform for bdist_egg
    refs #1

--- hatch_an_egg.sh
+++ hatch_an_egg.sh
@@ -4,7 +4,7 @@ cp ../cpp/ev3dev.{cpp,h} .
 
 if [ "$1" == "upload" ]; then
     # Create a binary egg and upload it to pypi
-    python setup.py bdist_egg upload
+    python setup.py bdist_egg -p linux-armv5tejl upload
 else
     # Install locally
     python setup.py install
