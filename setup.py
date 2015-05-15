e55c0a0b7d401dc0e152ba57bed812470aa6d288
Denis Demidov <dennis.demidov@gmail.com>
2015-05-15 18:33:04 +0300

    Use git version for docs revision

--- setup.py
+++ setup.py
@@ -1,56 +1,9 @@
 from setuptools import setup, Extension
-from subprocess import Popen, PIPE
 from spec_version import spec_version
+from git_version import git_version
 import os, sys
 
 pyver = sys.version_info
-
-#----------------------------------------------------------------------------
-# Get version string from git
-#
-# Author: Douglas Creager <dcreager@dcreager.net>
-# http://dcreager.net/2010/02/10/setuptools-git-version-numbers/
-#
-# PEP 386 adaptation from
-# https://gist.github.com/ilogue/2567778/f6661ea2c12c070851b2dfb4da8840a6641914bc
-#----------------------------------------------------------------------------
-def call_git_describe(abbrev=4):
-    try:
-        p = Popen(['git', 'describe', '--abbrev=%d' % abbrev],
-                  stdout=PIPE, stderr=PIPE)
-        p.stderr.close()
-        line = p.stdout.readlines()[0]
-        return line.strip().decode('utf8')
-
-    except:
-        return None
-
-
-def pep386adapt(version):
-    # adapt git-describe version to be in line with PEP 386
-    parts = version.split('-')
-    if len(parts) > 1:
-        parts[-2] = 'post'+parts[-2]
-        version = '.'.join(parts[:-1])
-    return version
-
-
-def get_git_version(abbrev=4):
-    # Try to get the current version using "git describe".
-    version = call_git_describe(abbrev)
-
-    if version is None:
-        raise ValueError("Cannot find the version number!")
-
-    # adapt to PEP 386 compatible versioning scheme
-    version = pep386adapt(version)
-
-    # write version to version.py
-    open("ev3dev/version.py", "w").write("__version__='%s (%s)'\n" % (version, spec_version))
-
-    # Finally, return the current version.
-    return version
-
 
 #----------------------------------------------------------------------------
 def find_file(filename, search_dirs):
@@ -91,11 +44,14 @@ def boost_python_lib():
 
     return "boost_python"
 
+# write version to version.py
+open("ev3dev/version.py", "w").write("__version__='%s (%s)'\n" % (git_version(), spec_version))
+
 
 #----------------------------------------------------------------------------
 setup(
         name='python-ev3dev',
-        version=get_git_version(),
+        version=git_version(),
         description='Python language bindings for ev3dev',
         author='Denis Demidov',
         author_email='dennis.demidov@gmail.com',
